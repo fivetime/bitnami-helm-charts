e9438336af92a5e1942ea1b1382205effe548fa7
Simon Zhou <jp.zdm2008@gmail.com>
2026-01-31 01:04:23 +0900

    Optimize MySQL schema: adjust column lengths, update unique key definitions, and ensure consistent table formatting.

--- bitnami/nacos/resources/schema/mysql.sql
+++ bitnami/nacos/resources/schema/mysql.sql
@@ -3,9 +3,9 @@
 -- https://github.com/alibaba/nacos
 
 CREATE TABLE IF NOT EXISTS `config_info` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                             `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
     `data_id` varchar(255) NOT NULL COMMENT 'data_id',
-    `group_id` varchar(255) DEFAULT NULL,
+    `group_id` varchar(128) DEFAULT NULL,
     `content` longtext NOT NULL COMMENT 'content',
     `md5` varchar(32) DEFAULT NULL COMMENT 'md5',
     `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
@@ -21,24 +21,24 @@ CREATE TABLE IF NOT EXISTS `config_info` (
     `c_schema` text,
     `encrypted_data_key` varchar(1024) NOT NULL DEFAULT '' COMMENT 'key',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_configinfo_datagrouptenant` (`data_id`,`group_id`,`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info';
+    UNIQUE KEY `uk_configinfo_datagrouptenant` (`data_id`(128),`group_id`,`tenant_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info';
 
 CREATE TABLE IF NOT EXISTS `config_info_aggr` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                                  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
     `data_id` varchar(255) NOT NULL COMMENT 'data_id',
-    `group_id` varchar(255) NOT NULL COMMENT 'group_id',
+    `group_id` varchar(128) NOT NULL COMMENT 'group_id',
     `datum_id` varchar(255) NOT NULL COMMENT 'datum_id',
     `content` longtext NOT NULL COMMENT 'content',
     `gmt_modified` datetime NOT NULL COMMENT 'modify time',
     `app_name` varchar(128) DEFAULT NULL,
     `tenant_id` varchar(128) DEFAULT '' COMMENT 'tenant',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_configinfoaggr_datagrouptenantdatum` (`data_id`,`group_id`,`tenant_id`,`datum_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info_aggr';
+    UNIQUE KEY `uk_configinfoaggr_datagrouptenantdatum` (`data_id`(128),`group_id`,`tenant_id`,`datum_id`(128))
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info_aggr';
 
 CREATE TABLE IF NOT EXISTS `config_info_beta` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                                  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
     `data_id` varchar(255) NOT NULL COMMENT 'data_id',
     `group_id` varchar(128) NOT NULL COMMENT 'group_id',
     `app_name` varchar(128) DEFAULT NULL COMMENT 'app_name',
@@ -52,11 +52,11 @@ CREATE TABLE IF NOT EXISTS `config_info_beta` (
     `tenant_id` varchar(128) DEFAULT '' COMMENT 'tenant',
     `encrypted_data_key` varchar(1024) NOT NULL DEFAULT '' COMMENT 'key',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_configinfobeta_datagrouptenant` (`data_id`,`group_id`,`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info_beta';
+    UNIQUE KEY `uk_configinfobeta_datagrouptenant` (`data_id`(128),`group_id`,`tenant_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info_beta';
 
 CREATE TABLE IF NOT EXISTS `config_info_tag` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                                 `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
     `data_id` varchar(255) NOT NULL COMMENT 'data_id',
     `group_id` varchar(128) NOT NULL COMMENT 'group_id',
     `tenant_id` varchar(128) DEFAULT '' COMMENT 'tenant_id',
@@ -70,11 +70,11 @@ CREATE TABLE IF NOT EXISTS `config_info_tag` (
     `src_ip` varchar(50) DEFAULT NULL COMMENT 'source ip',
     `encrypted_data_key` varchar(1024) NOT NULL DEFAULT '' COMMENT 'key',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_configinfotag_datagrouptenanttag` (`data_id`,`group_id`,`tenant_id`,`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info_tag';
+    UNIQUE KEY `uk_configinfotag_datagrouptenanttag` (`data_id`(128),`group_id`,`tenant_id`,`tag_id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info_tag';
 
 CREATE TABLE IF NOT EXISTS `config_info_gray` (
-    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                                  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
     `data_id` varchar(255) NOT NULL COMMENT 'data_id',
     `group_id` varchar(128) NOT NULL COMMENT 'group_id',
     `content` longtext NOT NULL COMMENT 'content',
@@ -89,13 +89,13 @@ CREATE TABLE IF NOT EXISTS `config_info_gray` (
     `gray_rule` text NOT NULL COMMENT 'gray_rule',
     `encrypted_data_key` varchar(256) NOT NULL DEFAULT '' COMMENT 'encrypted_data_key',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_configinfogray_datagrouptenantgray` (`data_id`,`group_id`,`tenant_id`,`gray_name`),
+    UNIQUE KEY `uk_configinfogray_datagrouptenantgray` (`data_id`(128),`group_id`,`tenant_id`,`gray_name`),
     KEY `idx_dataid_gmt_modified` (`data_id`,`gmt_modified`),
     KEY `idx_gmt_modified` (`gmt_modified`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info_gray';
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_info_gray';
 
 CREATE TABLE IF NOT EXISTS `config_tags_relation` (
-    `id` bigint(20) NOT NULL COMMENT 'id',
+                                                      `id` bigint(20) NOT NULL COMMENT 'id',
     `tag_name` varchar(128) NOT NULL COMMENT 'tag_name',
     `tag_type` varchar(64) DEFAULT NULL COMMENT 'tag_type',
     `data_id` varchar(255) NOT NULL COMMENT 'data_id',
@@ -105,10 +105,10 @@ CREATE TABLE IF NOT EXISTS `config_tags_relation` (
     PRIMARY KEY (`nid`),
     UNIQUE KEY `uk_configtagrelation_configidtag` (`id`,`tag_name`,`tag_type`),
     KEY `idx_tenant_id` (`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_tag_relation';
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='config_tag_relation';
 
 CREATE TABLE IF NOT EXISTS `group_capacity` (
-    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                                `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
     `group_id` varchar(128) NOT NULL DEFAULT '' COMMENT 'Group ID',
     `quota` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'quota',
     `usage` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'usage',
@@ -120,10 +120,10 @@ CREATE TABLE IF NOT EXISTS `group_capacity` (
     `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'modify time',
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_group_id` (`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='group_capacity';
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='group_capacity';
 
 CREATE TABLE IF NOT EXISTS `his_config_info` (
-    `id` bigint(20) unsigned NOT NULL COMMENT 'id',
+                                                 `id` bigint(20) unsigned NOT NULL COMMENT 'id',
     `nid` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'nid',
     `data_id` varchar(255) NOT NULL COMMENT 'data_id',
     `group_id` varchar(128) NOT NULL COMMENT 'group_id',
@@ -144,10 +144,10 @@ CREATE TABLE IF NOT EXISTS `his_config_info` (
     KEY `idx_gmt_create` (`gmt_create`),
     KEY `idx_gmt_modified` (`gmt_modified`),
     KEY `idx_did` (`data_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='his_config_info';
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='his_config_info';
 
 CREATE TABLE IF NOT EXISTS `tenant_capacity` (
-    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                                 `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
     `tenant_id` varchar(128) NOT NULL DEFAULT '' COMMENT 'Tenant ID',
     `quota` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'quota',
     `usage` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'usage',
@@ -159,10 +159,10 @@ CREATE TABLE IF NOT EXISTS `tenant_capacity` (
     `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'modify time',
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_tenant_id` (`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='tenant_capacity';
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='tenant_capacity';
 
 CREATE TABLE IF NOT EXISTS `tenant_info` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                             `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
     `kp` varchar(128) NOT NULL COMMENT 'kp',
     `tenant_id` varchar(128) DEFAULT '' COMMENT 'tenant_id',
     `tenant_name` varchar(128) DEFAULT '' COMMENT 'tenant_name',
@@ -173,23 +173,23 @@ CREATE TABLE IF NOT EXISTS `tenant_info` (
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_tenant_info_kptenantid` (`kp`,`tenant_id`),
     KEY `idx_tenant_id` (`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='tenant_info';
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='tenant_info';
 
 CREATE TABLE IF NOT EXISTS `users` (
-    `username` varchar(50) NOT NULL PRIMARY KEY COMMENT 'username',
+                                       `username` varchar(50) NOT NULL PRIMARY KEY COMMENT 'username',
     `password` varchar(500) NOT NULL COMMENT 'password',
     `enabled` boolean NOT NULL COMMENT 'enabled'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS `roles` (
-    `username` varchar(50) NOT NULL COMMENT 'username',
+                                       `username` varchar(50) NOT NULL COMMENT 'username',
     `role` varchar(50) NOT NULL COMMENT 'role',
     UNIQUE INDEX `idx_user_role` (`username` ASC, `role` ASC) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS `permissions` (
-    `role` varchar(50) NOT NULL COMMENT 'role',
+                                             `role` varchar(50) NOT NULL COMMENT 'role',
     `resource` varchar(255) NOT NULL COMMENT 'resource',
     `action` varchar(8) NOT NULL COMMENT 'action',
     UNIQUE INDEX `uk_role_permission` (`role`,`resource`,`action`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
